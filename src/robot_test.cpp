7240711766941de2016ae29e28f6f5af326ced8a
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-17 13:10:22 +1100

    Refactor initial Robot test, remove comments
    Refactor Robot tests to compare its expected output for its report
    method to the actual output that is captured and stored temporarily in a
    file to later be loaded back in for comparison.
    
    == Notes:
    - The following is an example of the issue caused when using
      `ASSERT_STREQ` without sub-stringing the stdout output that includes
      additional newline characters from the end of file
    
    ```console
    $ docker run \
      --workdir=/app \
      --volume "$(pwd)":/app \
      sonna/googletest \
      \
      sh -c "make test"
    
    g++ -c -o src/robot_test.o src/robot_test.cpp -g -Wall -std=c++11 -I.
    g++ -g -Wall -std=c++11 -I. -I/usr/local/lib/include/ -o bin/test src/robot.o src/robot_test.o src/test_helpers.o -lm -lgtest && \
    ./bin/test
    
    [==========] Running 1 test from 1 test case.
    [----------] Global test environment set-up.
    [----------] 1 test from RobotTest
    [ RUN      ] RobotTest.Report
    src/robot_test.cpp:12: Failure
    Expected equality of these values:
      stdout_contents
        Which is: "0,0,NORTH\n\x8F\x99\xF6\x7F"
      expected_output
        Which is: "0,0,NORTH\n"
    With diff:
    @@ -1,2 +1,1 @@
    -0,0,NORTH
    -\x8F\x99\xF6\x7F
    +0,0,NORTH\n
    
    [  FAILED  ] RobotTest.Report (4 ms)
    [----------] 1 test from RobotTest (4 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test case ran. (4 ms total)
    [  PASSED  ] 0 tests.
    [  FAILED  ] 1 test, listed below:
    [  FAILED  ] RobotTest.Report
    
     1 FAILED TEST
    
    make: *** [Makefile:44: test] Error 1
    ```

--- src/robot_test.cpp
+++ src/robot_test.cpp
@@ -3,15 +3,19 @@
 #include "src/robot.h"
 
 TEST(RobotTest, Report) {
-    // ToyRobot::Robot* subject = new ToyRobot::Robot();
-
-    // const char* stdout_contents = capture_output(subject->report);
     ToyRobot::Robot subject;
     auto function_pointer = std::bind(&ToyRobot::Robot::report, subject);
-    // function1(fp);
 
     const char* stdout_contents = capture_output(function_pointer);
-    // const char* stdout_contents = capture_output(subject.report);
+    const char* expected_output = "0,0,NORTH\n";
+    EXPECT_EQ(0, strncmp(stdout_contents, expected_output, strlen(expected_output)));
+}
+
+TEST(RobotTest, ReportFromDereferenceSubjectPointer) {
+    ToyRobot::Robot* subject = new ToyRobot::Robot();
+    auto function_pointer = std::bind(&ToyRobot::Robot::report, *subject);
+
+    const char* stdout_contents = capture_output(function_pointer);
     const char* expected_output = "0,0,NORTH\n";
     EXPECT_EQ(0, strncmp(stdout_contents, expected_output, strlen(expected_output)));
 }
